ec9b3ac0777a377df316766b5d4b411ecd49c2cd
Yovel Key-Cohen <ykey-cohen@emeryweiner.org>
2023-02-07 18:28:55 -0600

    doping works, returns percentages

--- doping.py
+++ doping.py
@@ -1,32 +1,59 @@
 import numpy as np
-
-length = 3
-bonds = np.ones((length, length, length, 3), np.int8)
+from random import randint
 
 
-def boolv(x,y,z):
-    return np.array((x,y,z), dtype=bool)
+def make_bonds_array(dimx, dimy, dimz):
+    return np.ones((dimx, dimy, dimz, 3), np.int8)
 
-def dope(bonds, x,y,z):
-    X = np.array((1, 0, 0))
-    Y = np.array((0, 1, 0))
-    Z = np.array((0, 0, 1))
-
-    center = np.array((x,y,z))
-
-    for point, val in np.ndenumerate(bonds[x:x+2,y:y+2,z:z+2]):
-        #print(point)
-        bonds[point] = np.multiply(val, (point[:-1]-center)[point[-1]])
-
-    #bonds[point] = np.multiply(bonds[point], (0,0,0))
-    #bonds[point+X] = np.multiply(bonds[point+X], (1,0,0))
-    #bonds[point+Y] = np.multiply(bonds[point+Y], (0,1,0))
-    #bonds[point+Z] = np.multiply(bonds[point+Z], (0,0,1))
-    #bonds[point+X+Z] = np.multiply(bonds[point+X+Z], (1,0,1))
-    #bonds[point+Y+Z] = np.multiply(bonds[point+Y+Z], (0,1,1))
-    #bonds[point+X+Y] = np.multiply(bonds[point+X+Y], (1,1,0))
+def dope(arr, x,y,z):
+    doper = np.array([[[[0., 0., 0.],
+                        [0., 0., 1.]],
+                       [[0., 1., 0.],
+                        [0., 1., 1.]]],
+                      [[[1., 0., 0.],
+                        [1., 0., 1.]],
+                       [[1., 1., 0.],
+                        [1., 1., 1.]]]], dtype=np.int8)
+    arr[x:x + 2, y:y + 2, z:z + 2] *= doper
 
 
-dope(bonds, 0,0,0)
+def to_vertices(edges):
+    disp_picker = np.identity(3, dtype=np.int8)
 
-print(bonds)
+    vertices = np.zeros(edges.shape[:3], dtype=np.int8)
+    for p, val in np.ndenumerate(edges):
+        coord = p[:3]
+        #print(p, val, coord, disp_picker[p[3]])
+        vertices[coord] += val
+        try:
+            vertices[tuple(coord+disp_picker[p[3]])] += val
+        except IndexError:
+            pass
+
+    return vertices
+
+
+def random_dope(arr, count):
+    for _ in range(count):
+        dope(arr, randint(1, arr.shape[0]-2),randint(1, arr.shape[1]-2),randint(1, arr.shape[2]-2))
+
+
+def produce_sample(length, doping_count):
+    length = length+1
+    bonds = make_bonds_array(length, length, length)
+    random_dope(bonds, doping_count)
+    return to_vertices(bonds)[1:,1:,1:]
+
+def count_polymers(vertex_arr):
+    return list(zip(*np.unique(vertex_arr, return_counts=True)))
+
+def count_polymer_pct(vertex_arr):
+    total = np.prod(vertex_arr.shape)
+    counts = count_polymers(vertex_arr)
+    for i in range(len(counts)):
+        counts[i] = (counts[i][0], counts[i][1]/total)
+    return counts
+
+
+sample: np.ndarray = produce_sample(100, 1000000//10)
+print(count_polymer_pct(sample))
